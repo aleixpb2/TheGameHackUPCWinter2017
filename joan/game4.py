6d5f1bd8c3544dccf98f2f665cf6c72d80b549d3
jmigual <j.marce.igual@gmail.com>
2017-03-04 10:20:22 +0100

    Fix some coordinate issues

--- joan/game4.py
+++ joan/game4.py
@@ -14,10 +14,10 @@ class Block:
 
 
 def create_block(pixels, i, j):
-    l = pixels[i + 3, j]
-    r = pixels[i + 3, j + 7]
-    t = pixels[i, j + 3]
-    b = pixels[i + 7, j + 3]
+    l = pixels[i, j + 3]
+    r = pixels[i + 7, j + 3]
+    t = pixels[i + 3, j]
+    b = pixels[i + 3, j + 7]
     color = pixels[i, j]
     return Block(l, r, t, b, color)
 
@@ -40,11 +40,15 @@ def find_solution_top(reference, blocks):
     row_b = len(blocks[0])
     col = reference.b
 
+    found = None
     for i in range(col_b):
         for j in range(row_b):
             if blocks[i][j].t == col:
                 print("Found at: ", i, j)
-                return blocks[i][j]
+                if not found is None:
+                    print("Ep! caca")
+                found = blocks[i][j]
+    return found
 
 
 def main():
@@ -55,40 +59,53 @@ def main():
     block_rows = int(row / 8)
 
     blocks = []
+    image_base = Image.new('RGB', (block_rows, block_cols))
+    image_col = Image.new('RGB', (block_rows, block_cols))
 
     for i in range(0, row, 8):
         aux = []
         for j in range(0, col, 8):
             r = int(i / 8)
             c = int(j / 8)
-            aux.append(create_block(pixels, i, j))
+            block = create_block(pixels, i, j)
+            aux.append(block)
         blocks.append(aux)
+
+    for i in range(block_rows):
+        for j in range(block_cols):
+            image_base.putpixel((i, j), blocks[i][j].c)
 
     solution = []
     pixels = []
     image = Image.new('RGB', (block_rows, block_cols))
-    imageBase = Image.new('RGB', (block_rows, block_cols))
+    image_debug = Image.new('RGB', (3*block_rows, 3*block_cols))
 
-    for i in range(block_cols):
+    for i in range(block_rows):
         aux = []
-        for j in range(block_rows):
+        for j in range(block_cols):
             aux_sol = None
-            print("Finding: ", i, j)
-            if i < 8 and j < 8:
+            print("Finding x:", i, "y:", j)
+            if i < 7 and j < 7:
                 print("Base block")
                 aux_sol = blocks[i][j]
             else:
-                if i < 8:
+                if i < 7:
                     aux_sol = find_solution_left(aux[j - 1], blocks)
                 else:
                     aux_sol = find_solution_top(solution[i - 1][j], blocks)
             aux.append(aux_sol)
-            image.putpixel((j, i), aux_sol.c)
-            imageBase.putpixel((j, i), blocks[i][j].c)
+            image.putpixel((i, j), aux_sol.c)
+            image_debug.putpixel((3*i + 1, 3*j + 1), aux_sol.c)
+            image_debug.putpixel((3 * i + 1, 3 * j), aux_sol.l)
+            image_debug.putpixel((3 * i + 1, 3 * j + 2), aux_sol.r)
+            image_debug.putpixel((3 * i, 3 * j + 1), aux_sol.t)
+            image_debug.putpixel((3 * i + 2, 3 * j + 1), aux_sol.b)
         solution.append(aux)
 
     image.save("prova.png")
-    imageBase.save("base.png")
+    image_base.save("base.png")
+    image_col.save("color.png")
+    image_debug.save("debug.png")
 
 if __name__ == "__main__":
     main()
